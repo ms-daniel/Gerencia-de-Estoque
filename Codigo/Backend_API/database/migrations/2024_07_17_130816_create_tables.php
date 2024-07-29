5c1ffd81af2c00d362b40374e9f80a5ee5bba78e
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 16:27:05 -0300

    fix!: store id in item, price in stock and just stock id in stock movements

--- Codigo/Backend_API/database/migrations/2024_07_17_130816_create_tables.php
+++ Codigo/Backend_API/database/migrations/2024_07_17_130816_create_tables.php
@@ -39,7 +39,7 @@ return new class extends Migration
             $table->string('phone')->nullable();
             $table->string('email')->unique()->nullable();
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users_profiles')->onDelete('cascade');;
+            $table->foreign('user_id')->references('id')->on('users_profiles')->onDelete('cascade');
         });
 
         Schema::create('categories', function (Blueprint $table) {
@@ -59,12 +59,13 @@ return new class extends Migration
             $table->string('code')->unique();
             $table->string('name');
             $table->string('description')->nullable();
-            $table->decimal('price', total: 10, places: 2)->default(0.0);
             $table->unsignedBigInteger('supplier_id');
             $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('store_id');
             $table->timestamps();
             $table->foreign('supplier_id')->references('id')->on('suppliers');
             $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
         });
 
         Schema::create('item_subcategories', function (Blueprint $table) {
@@ -79,21 +80,18 @@ return new class extends Migration
         Schema::create('stocks', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('item_id');
-            $table->unsignedBigInteger('store_id');
+            $table->decimal('price', total: 10, places: 2)->default(0.0);
             $table->timestamps();
             $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
-            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
         });
 
         Schema::create('stock_movements', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('item_id');
-            $table->unsignedBigInteger('store_id');
+            $table->unsignedBigInteger('stock_id');
             $table->enum('movement_type', ['entrada', 'saida']);
             $table->integer('quantity');
             $table->timestamps();
-            $table->foreign('item_id')->references('id')->on('items')->onDelete('cascade');
-            $table->foreign('store_id')->references('id')->on('stores')->onDelete('cascade');
+            $table->foreign('stock_id')->references('id')->on('items')->onDelete('cascade');
         });
     }
 
@@ -102,14 +100,14 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users_profiles');
-        Schema::dropIfExists('stores');
-        Schema::dropIfExists('suppliers');
-        Schema::dropIfExists('categories');
-        Schema::dropIfExists('sub_categories');
-        Schema::dropIfExists('items');
         Schema::dropIfExists('item_subcategories');
         Schema::dropIfExists('stocks');
         Schema::dropIfExists('stock_movements');
+        Schema::dropIfExists('items');
+        Schema::dropIfExists('sub_categories');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('suppliers');
+        Schema::dropIfExists('stores');
+        Schema::dropIfExists('users_profiles');
     }
 };
