d33ca3085b8e5bd2d8357f0fd2e640541e3a9310
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 16:25:36 -0300

    model #3

--- Codigo/Backend_API/app/Models/Item.php
+++ Codigo/Backend_API/app/Models/Item.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Item extends Model
 {
+    protected $fillable = ['code', 'name', 'description', 'supplier_id', 'category_id', 'store_id'];
+
+    public function stocks(): HasMany
+    {
+        return $this->hasMany(Stock::class);
+    }
+
+    public function itemSubcategories(): HasMany
+    {
+        return $this->HasMany(ItemSubcategory::class);
+    }
+
+    public function category(): HasOne
+    {
+        return $this->HasOne(Category::class);
+    }
+
     use HasFactory;
 }
