a95f97c72772fbdd88336ee0bca429dc8c4a59b4
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 11:58:40 -0300

    controller #1

---
+++ Codigo/Backend_API/app/Http/Controllers/StoreController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use App\Interfaces\IStoreService;
+use App\Http\Requests\StoreCreateRequest;
+
+class StoreController extends Controller
+{
+    protected $storeService;
+
+    protected $createRules = [
+        'name' => 'required|string|max:255',
+        'street' => 'nullable|string|max:255',
+        'city' => 'nullable|string|max:255',
+        'state' => 'required|string|max:255',
+        'country' => 'required|string|max:255',
+        'postal_code' => 'required|string|max:20',
+        'user_id' => 'required|integer|exists:users_profiles,id',  // Ajuste para o nome correto da tabela
+    ];
+
+    protected $updateRules = [
+        'name' => 'sometimes|required|string|max:255',
+        'street' => 'sometimes|string|max:255',
+        'city' => 'sometimes|string|max:255',
+        'state' => 'sometimes|required|string|max:255',
+        'country' => 'sometimes|required|string|max:255',
+        'postal_code' => 'sometimes|required|string|max:20',
+    ];
+
+    public function __construct(IStoreService $storeService)
+    {
+        $this->storeService = $storeService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = $this->storeService->getAll();
+        return response()->json([
+            'status' => true,
+            'message' => 'Stores retrieved successfully',
+            'data' => $users
+        ],200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), $this->createRules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $data = $validator->validated();
+
+        $store = $this->storeService->create($data);
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Store created successfully',
+            'data' => $store
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $user = $this->storeService->get($id);
+
+        if(!$user) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Store not found',
+            ], 404);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Store found successfully',
+            'data' => $user
+        ], 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+        $store = $this->storeService->update($id, $data);
+
+        if (!$store) {
+            return response()->json([
+                'status' => false,
+                'message' => 'Store not found'
+            ],404);
+        } else {
+            return response()->json([
+                'status' => true,
+                'message' => 'Updated!'
+            ], 200);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $deleted = $this->storeService->delete($id);
+
+        if(!$deleted){
+            return response()->json([
+                'status' => false,
+                'message' => 'Some error occurred'
+            ],500);
+        }
+
+        return response()->json([
+            'status' => true,
+            'message' => 'Store deleted!'
+        ],200);
+    }
+}
