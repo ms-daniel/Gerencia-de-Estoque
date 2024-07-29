6b58e9fb5a1ef4438c56748f971e05c9a4813a1c
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 12:19:26 -0300

    model #2

--- Codigo/Backend_API/app/Models/Supplier.php
+++ Codigo/Backend_API/app/Models/Supplier.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Supplier extends Model
 {
+    protected $fillable = ['name', 'phone', 'email', 'user_id'];
+
+    public function userProfile(): HasOne
+    {
+        return $this->hasOne(UsersProfile::class);
+    }
+
     use HasFactory;
 }
