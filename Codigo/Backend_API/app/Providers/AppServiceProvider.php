9b7d8da5be5730831539cd7dc45cc37c3a753c10
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 12:19:17 -0300

    dependency injection #2

--- Codigo/Backend_API/app/Providers/AppServiceProvider.php
+++ Codigo/Backend_API/app/Providers/AppServiceProvider.php
@@ -4,10 +4,13 @@ namespace App\Providers;
 
 use App\Interfaces\IStoreService;
 use App\Interfaces\IUsersProfileService;
+use App\Interfaces\ISupplierService;
 
 use App\Services\UsersProfileService;
 use App\Services\StoreService;
+use App\Services\SupplierService;
 
+use App\Models\Supplier;
 use App\Models\Store;
 use App\Models\UsersProfile;
 use Illuminate\Support\ServiceProvider;
@@ -22,13 +25,16 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(UsersProfileService::class, function($app){
             return new UsersProfileService($app->make(UsersProfile::class));
         });
-
         $this->app->singleton(StoreService::class, function($app){
             return new StoreService($app->make(Store::class));
+        });
+        $this->app->singleton(SupplierService::class, function($app){
+            return new SupplierService($app->make(Supplier::class));
         });
 
         $this->app->bind(IUsersProfileService::class, UsersProfileService::class);
         $this->app->bind(IStoreService::class, StoreService::class);
+        $this->app->bind(ISupplierService::class, SupplierService::class);
     }
 
     /**
