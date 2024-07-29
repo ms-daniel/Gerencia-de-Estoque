4ceb710f5fb25041c2ac436116b93576c9ba4cac
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 11:58:00 -0300

    routes #1

--- Codigo/Backend_API/routes/api.php
+++ Codigo/Backend_API/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\UsersProfileController;
+use App\Http\Controllers\StoreController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::apiResource('usersprofile', UsersProfileController::class);
+Route::apiResource('store', StoreController::class);
