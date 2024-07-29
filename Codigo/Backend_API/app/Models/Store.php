2f654502a78ab06f8d7f5ab4a261cdebbf90067d
Carlos Daniel <ms_danniel@outlook.com>
2024-07-29 11:58:21 -0300

    model #1

--- Codigo/Backend_API/app/Models/Store.php
+++ Codigo/Backend_API/app/Models/Store.php
@@ -5,7 +5,16 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 class Store extends Model
 {
+    protected $fillable = ['name', 'street', 'city', 'state', 'country', 'postal_code', 'user_id'];
+
+    public function userProfile(): HasOne
+    {
+        return $this->hasOne(UsersProfile::class);
+    }
+
     use HasFactory;
 }
