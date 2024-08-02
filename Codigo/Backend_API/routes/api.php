9d8792eb5de536253744ff500a6bc7ce7533b1b9
Carlos Daniel <ms_danniel@outlook.com>
2024-08-02 16:22:12 -0300

    feat: routes with authentication #6

--- Codigo/Backend_API/routes/api.php
+++ Codigo/Backend_API/routes/api.php
@@ -1,24 +1,35 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ItemController;
 use App\Http\Controllers\StockController;
 use App\Http\Controllers\StoreController;
 use App\Http\Controllers\SubcategoryController;
 use App\Http\Controllers\SupplierController;
-use App\Http\Controllers\UserProfileController;
+use App\Http\Controllers\UserController;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::middleware(['auth:sanctum', 'abilities:admin'])->group(function () {
+    Route::apiResource('user', UserController::class);
+    Route::apiResource('category', CategoryController::class);
+    Route::apiResource('subcategory', SubcategoryController::class);
+});
 
-Route::apiResource('userProfile', UserProfileController::class);
-Route::apiResource('store', StoreController::class);
-Route::apiResource('supplier', SupplierController::class);
-Route::apiResource('item', ItemController::class);
-Route::apiResource('category', CategoryController::class);
-Route::apiResource('subcategory', SubcategoryController::class);
-Route::apiResource('stock', StockController::class);
+Route::middleware(['auth:sanctum', 'ability:admin,user'])->group(function () {
+    Route::apiResource('store', StoreController::class)->middleware(['auth:sanctum', 'ability:admin, user']);
+    Route::apiResource('supplier', SupplierController::class)->middleware(['auth:sanctum', 'ability:admin, user']);
+    Route::apiResource('item', ItemController::class)->middleware(['auth:sanctum', 'ability:admin, user']);
+    Route::apiResource('stock', StockController::class)->middleware(['auth:sanctum', 'ability:admin, user']);
+    Route::get('tokens', [UserController::class, 'getTokens']);
+    Route::delete('tokens', [UserController::class, 'deleteTokens']);
+    Route::delete('tokens/all', [UserController::class, 'clearTokens']);
+});
+
+
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
