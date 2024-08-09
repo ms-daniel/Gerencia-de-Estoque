a0aa0fb31aa85316bef3f5a8f138f1139ba1224c
Carlos Daniel <ms_danniel@outlook.com>
2024-08-09 15:59:32 -0300

    routes and layout #11

--- Codigo/Frontend/src/app/app.routes.ts
+++ Codigo/Frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent},
+    { path: 'login', component: LoginComponent}
+];
