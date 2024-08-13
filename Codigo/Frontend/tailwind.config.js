05cb6e31f36d44a2bcf742b678aca8b360175715
Carlos Daniel <ms_danniel@outlook.com>
2024-08-13 15:59:56 -0300

    flowbite and apexchart #11

--- Codigo/Frontend/tailwind.config.js
+++ Codigo/Frontend/tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
