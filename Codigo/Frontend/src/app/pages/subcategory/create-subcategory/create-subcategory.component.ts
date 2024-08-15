47c6048f94d01289f010628360646bee4aad01f7
Carlos Daniel <ms_danniel@outlook.com>
2024-08-15 12:22:45 -0300

    list and create subcategory #13

---
+++ Codigo/Frontend/src/app/pages/subcategory/create-subcategory/create-subcategory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-subcategory',
+  standalone: true,
+  imports: [],
+  templateUrl: './create-subcategory.component.html',
+  styleUrl: './create-subcategory.component.css'
+})
+export class CreateSubcategoryComponent {
+
+}
---
+++ Codigo/Frontend/src/app/pages/subcategory/list-subcategory/list-subcategory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-subcategory',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-subcategory.component.html',
+  styleUrl: './list-subcategory.component.css'
+})
+export class ListSubcategoryComponent {
+
+}
